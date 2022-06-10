c0bf60cadde3b496686088998bab2676cb7b42bc
andrzej.zmudzinski <jendrusha@gmail.com>
2022-06-10 07:43:23 +0200

    feat: do not load inlay hints from rust ftplugin

--- after/ftplugin/rust.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_augroup("RustGroup", { clear = true })
-vim.api.nvim_create_autocmd({ "CursorMoved", "InsertLeave", "BufEnter", "BufWinEnter", "BufWritePost", "TabEnter" }, {
-	group = "RustGroup",
-	buffer = 0,
-	callback = function()
-		require("lsp_extensions").inlay_hints({ prefix = " » ", aligned = true, highlight = "Comment"})
-	end,
-})
